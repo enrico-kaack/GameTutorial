64dee80b5cf6bc4799b274fa1003606ead95cbf2
enrico-kaack <e.kaack@live.de>
2019-05-13 14:56:13 +0200

    Edited: collision property for the enemy

--- Bullet.gd
+++ Bullet.gd
@@ -6,6 +6,8 @@ var damage = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	collision_layer = 2
+	collision_mask = 2
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
